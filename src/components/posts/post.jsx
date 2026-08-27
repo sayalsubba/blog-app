b55201f4f6386fe7a28c3cd45782f1d07ab9fd2f
Sayal Subba <sahilphago23@gmail.com>
2026-08-27 16:43:59 +0545

    adding edit

--- src/components/posts/post.jsx
+++ src/components/posts/post.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Css from "./post.module.css"
 import { h1 } from 'motion/react-client'
+import { motion } from 'motion/react'
 export default function post({
     post,
     setPost
@@ -8,15 +9,25 @@ export default function post({
     return (
         <>
             <div className={Css["main"]}>
-                <div >
-                    {post.map((item) => (
-                       <div className={Css['mainPosts']}>
-                        <h1>Title: {item.title}</h1>
-                        <p>Author: {item.author}</p>
-                        <p>Description:{item.description}</p>
+
+                {post.map((item) => (
+                    <div className={Css["mainPrt"]}>
+                        <div className={Css['mainPosts']}>
+                            <h1>Title: {item.title}</h1>
+                            <p>Author: {item.author}</p>
+                            <p>Description:{item.description}</p>
+
                         </div>
-                    ))}
-                </div>
+                        <div className={Css["btn"]}>
+                            <motion.button
+                        whileHover={{scale:1.1}}
+                        whileTap={{scale:0.9}}
+                            >Edit</motion.button>
+                        </div>
+                    </div>
+
+                ))}
+
             </div>
         </>
     )
